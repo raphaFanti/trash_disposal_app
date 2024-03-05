4ee9006c261e737e163302ce664756f60a34ff9e
Raphael Fanti <raphael.fanti@gmail.com>
2024-03-05 15:19:40 +0100

    Implemented form for confirm_inputs route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import os
-from flask import Flask, render_template, request, redirect, flash, url_for, Response
-from forms import UploadForm
+from flask import Flask, render_template, request, redirect, flash, session, url_for, Response
+from forms import UploadForm, ConfirmInputForm
 from werkzeug.utils import secure_filename
 from gVision_functions import get_main_objects
 from gtp_functions import get_disposable_item
@@ -20,7 +20,7 @@ comuni = [c["nome"] for c in comuni_full]
 def autocomplete():
     return Response(json.dumps(comuni), mimetype='application/json')
 
-# Define routes and logic here
+# Views
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = UploadForm() #try request.form
@@ -32,7 +32,7 @@ def index():
         extension_allowed = image.filename.rsplit('.', 1)[1].lower() in allowed_image_extensions
 
         if not extension_allowed:
-            flash("File type not supported")
+            flash("File type not supported") #ToDo: implement flash messages printing
             return redirect(url_for('index'))
 
         # Saves image
@@ -44,12 +44,30 @@ def index():
 
         # Figues out if there is a disposable object in the image
         disposable_object = get_disposable_item(image_objects, key=app.config['OPENAI_KEY'])
-        # Deletes image
+        
+        # ToDo: Manage file deletion / persistence
         #os.remove(image_path)
 
         if disposable_object != "I don't know":
-            return render_template("confirm_inputs.html", image_url=image_path, item_to_dispose=disposable_object)
+            session['image_url'] = image_path
+            session['item_to_dispose'] = disposable_object
+            return redirect(url_for('confirm_inputs'))
         else:
+            # ToDo: To be implemented
             return render_template("problem.html", error_message=disposable_object)
         
     return render_template('index.html',  form=form)
+
+@app.route('/confirm_inputs', methods=['GET', 'POST'])
+def confirm_inputs():
+    form = ConfirmInputForm()
+
+    if form.validate_on_submit():
+        if form.input_field != "":
+            return redirect(url_for('index'))
+        else:
+            return render_template("problem.html", error_message="test no change in text")
+        
+    return render_template("confirm_inputs.html", image_url=session["image_url"], item_to_dispose=session["item_to_dispose"], form=form)
+
+#@app.route('/problem', methods=['POST'])
--- forms.py
+++ forms.py
@@ -6,3 +6,7 @@ class UploadForm(FlaskForm):
     image = FileField("Carica l'imagine di quello che vuoi buttare", validators=[DataRequired()], id = "image")
     name = StringField("In quale comune d'Italia sei?", validators=[DataRequired(), Length(4, 40)], id = "comune_autocomplete")
     submit = SubmitField('Invia')
+
+class ConfirmInputForm(FlaskForm):
+    input_field = StringField('Cosa vuoi buttare?', validators=[Length(0, 40)], id = 'input_field')
+    confirm_input = SubmitField('Conferma', id = 'confirm_input')
